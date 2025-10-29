d38e30e7169334a79dcf097f1b4c0b4d8e031e51
Iago Lima <iagoclima22@gmail.com>
2025-10-29 11:58:58 -0300

    Uptade fixaxao.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int y = 32;
-		
-	    System.out.println(y);	
 		System.out.println("Olá mundo!");
 
 	}
--- src/fixacao.java
+++ src/fixacao.java
@@ -1,8 +1,34 @@
+import java.util.Locale;
 
 public class fixacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String product1 = "Computer";
+		String product2 = "Office desl";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 52.234567;
+		
+		System.out.println("Products:");
+		System.out.println(product1 + ", whick price is $" + price1);
+		System.out.println(product2 + ", whick price is $" + price2);
+		
+		System.out.println();
+		
+		System.out.println("Record: " + age + " years old, code " + code + " and gender: " + gender);
+		
+		System.out.println();
+		
+		System.out.println("Measure with eight decimal places: " + measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f", measure);
 
 	}
 
