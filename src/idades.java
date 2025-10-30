3338e7ecb26172ab51d8e08ddc29462ec88393a1
Iago Lima <iagoclima22@gmail.com>
2025-10-29 21:11:11 -0300

    Criado idades.java - Exercício feito

---
+++ src/idades.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class idades {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome1, nome2;
+		int idade1, idade2;
+		double media;
+		
+		System.out.println("Dados da primeira pessoa:");
+		System.out.print("Nome: ");
+		nome1 = sc.nextLine();
+		System.out.print("Idade: ");
+		idade1 = sc.nextInt();
+		
+		System.out.println("Dados da segunda pessoa:");
+		sc.nextLine();
+		System.out.print("Nome: ");
+		nome2 = sc.nextLine();
+		System.out.print("Idade: ");
+		idade2 = sc.nextInt();
+		
+		media = (double) (idade1 + idade2) / 2;
+		
+		System.out.printf("A idade média de %s e %s é de %.1f anos", nome1, nome2, media);
+		
+		sc.close();
+		
+	}
+
+}
