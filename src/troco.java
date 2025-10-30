4e577315e9ae58adef0e9d9d26ccb2c58d30b656
Iago Lima <iagoclima22@gmail.com>
2025-10-29 21:29:42 -0300

    Criado troco.java - Exercício feito

---
+++ src/troco.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class troco {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double troco, preco, dinheiroRecebido;
+		int qtd;
+		
+		System.out.print("Preço unitário do produto: ");
+		preco = sc.nextDouble();
+		System.out.print("Quantidade comprada: ");
+		qtd = sc.nextInt();
+		System.out.print("Dinheiro recebido: ");
+		dinheiroRecebido = sc.nextDouble();
+		
+		troco = dinheiroRecebido - (qtd * preco);
+		System.out.printf("TROCO = %.2f", troco);
+		
+		
+	}
+
+}
