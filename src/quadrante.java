7c09b95a262778e5283dbb2cc7a3134b8e9747b8
Iago Lima <iagoclima22@gmail.com>
2025-11-03 17:01:16 -0300

    Criado quadrante.java

---
+++ src/quadrante.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class quadrante {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite os valores das coordenadas X e Y:");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		while (x != 0 && y != 0) {
+			if (x > 0 && y > 0) {
+				System.out.println("QUADRANTE Q1");
+			} 
+			else if (x < 0 && y > 0) {
+				System.out.println("QUADRANTE Q2");
+			} 
+			else if (x < 0 && y < 0) {
+				System.out.println("QUADRANTE Q3");
+			} 
+			else {
+				System.out.println("QUADRANTE Q4");
+			}
+			
+			System.out.println("Digite os valores das coordenadas X e Y:");
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+
+		
+		
+		sc.close();
+		
+	}
+
+}
