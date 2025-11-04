ff6ddec1b651d16be6d18b9c2f42866cf0252cef
Iago Lima <iagoclima22@gmail.com>
2025-11-03 21:39:31 -0300

    Criado sequencia_impares.java

---
+++ src/sequencia_impares.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class sequencia_impares {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o valor de x: ");
+		int x = sc.nextInt();
+		
+		for (int i = 1; i < +x; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+		sc.close();
+		
+	}
+
+}
