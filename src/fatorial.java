8544805e67035916e54bec01211e5af1b7f62288
Iago Lima <iagoclima22@gmail.com>
2025-11-04 07:21:39 -0300

    Criado fatorial.java

---
+++ src/fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fatorial {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o valor de N: ");
+		int n = sc.nextInt();
+		
+		int fat = 1;
+		for (int i = 1; i <= n; i++) {
+			fat = fat * i; 
+		}
+		
+		System.out.println("FATORIAL = " + fat);
+		
+		
+		sc.close();
+	}
+
+}
