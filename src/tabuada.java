d38a224c49be49b27b6f6c46b7dd894cfccc2ec9
Iago Lima <iagoclima22@gmail.com>
2025-11-03 21:22:36 -0300

    Criado tabuada.java

---
+++ src/tabuada.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class tabuada {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Deseja a tabuada para qual valor? ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(n + " x " + i + " = " + n * i);
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
